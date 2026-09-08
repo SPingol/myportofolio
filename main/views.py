d1d0fe0cbfd6284b01ab937505a2251c5fb70226
SPingol <serafin.reysetyo@ui.ac.id>
2026-09-08 23:21:13 +0700

    TUTORIAL2 ADDITIONS DEFAULT Tutorial 2 additions, following the exact instructions with minimal customisation added (TBA later)

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Serafin Reysetyo Amantresno Grajo Pingol",
+        "npm": "2506637136",
+        "study_program": "S1 Ilmu Komputer KKI",
+        "bio": (
+            "A Computer Science student at Universitas Indonesia interested "
+            "in software development and education. Truth Nuke"
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Burhan",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
