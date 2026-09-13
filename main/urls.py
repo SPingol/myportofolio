5208eac4d1436a3b2d8839217acbc4ab3a53834e
SPingol <serafin.reysetyo@ui.ac.id>
2026-09-13 17:58:38 +0700

    URLS Update Honestly forgor to this one earlier before doing the test rn

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from main.views import (
+    show_main,
+    show_experience,
+    show_competition,
+    show_education,
+)
 
-from main.views import show_main, show_experience
-
-app_name = "main"
+app_name = 'main'
 
 urlpatterns = [
-    path("", show_main, name="show_main"),
-    path("experience/", show_experience, name="show_experience"),
+    path('', show_main, name='show_main'),
+    path('experience/', show_experience, name='show_experience'),
+    path('competition/', show_competition, name='show_competition'),
+    path('education/', show_education, name='show_education'),
 ]
